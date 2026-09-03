e3ebe182f048add3cc15136d4bab8b9027d69827
GitHub <noreply@github.com>
2026-09-03 01:01:40 +0000

    Latest build deployed.

---
+++ lunr-pretext-search-index.js
@@ -0,0 +1,185 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "shorttitlelowercase-2",
+  "level": "1",
+  "url": "shorttitlelowercase-2.html",
+  "type": "Worksheet",
+  "number": "1",
+  "title": "Properties of Probability",
+  "body": " Properties of Probability   Random Experiment   An experiment where the outcome cannot be predicted with certainty. xyz     Sample Space   The set of all possible outcomes of a random experiment is called a sample space, S (also called outcome space, or just space).      Flip a coin and record the face that is showing:      Roll two dice and add the faces showing:       Flip two coins. Record the faces:      Flip two coins. Determine if they are the same:      Flip a coin until a heads appears. Record the number of flips needed:      Flip a coin until you get two heads in a row. Record how long (time) it takes:     Event   An event is a collection of outcomes in a sample space.    Notice that a sample space S is a set, and an event is a subset of S. We will review some basics of sets and see how they can be represented with Venn Diagrams. Our universal set is always our sample space in this context because the sample space contains all possible outcomes. We will assume all sets and elements are in S.   Empty or Null Set    denotes the empty or null set (the set containing no elements).       B indicates A is a subset of B. Draw and shade set A so it is a subset of B.          is the union of sets A and B. Elements in the union might be in A or in B or in both A and B. Shade           is the intersection of A and B. Elements in the intersection are in A and also in B. Shade .         A' is the complement of A (all elements in S that are NOT in A). Shade A'.          and are mutually exclusive or disjoint if . Make a sketch.         A and B are exhaustive if Make a sketch.       These definitions extend to larger numbers of subsets. For example, given     are mutually exclusive events if for (they are pairwise disjoint).     are exhaustive events if      "
+},
+{
+  "id": "def-ranexp",
+  "level": "2",
+  "url": "shorttitlelowercase-2.html#def-ranexp",
+  "type": "Definition",
+  "number": "1.1",
+  "title": "Random Experiment.",
+  "body": " Random Experiment   An experiment where the outcome cannot be predicted with certainty. xyz   "
+},
+{
+  "id": "def-samplespace",
+  "level": "2",
+  "url": "shorttitlelowercase-2.html#def-samplespace",
+  "type": "Definition",
+  "number": "1.2",
+  "title": "Sample Space.",
+  "body": " Sample Space   The set of all possible outcomes of a random experiment is called a sample space, S (also called outcome space, or just space).   "
+},
+{
+  "id": "shorttitlelowercase-2-4",
+  "level": "2",
+  "url": "shorttitlelowercase-2.html#shorttitlelowercase-2-4",
+  "type": "Worksheet Exercise",
+  "number": "1.1",
+  "title": "",
+  "body": "  Flip a coin and record the face that is showing:   "
+},
+{
+  "id": "shorttitlelowercase-2-5",
+  "level": "2",
+  "url": "shorttitlelowercase-2.html#shorttitlelowercase-2-5",
+  "type": "Worksheet Exercise",
+  "number": "1.2",
+  "title": "",
+  "body": "  Roll two dice and add the faces showing:    "
+},
+{
+  "id": "shorttitlelowercase-2-6",
+  "level": "2",
+  "url": "shorttitlelowercase-2.html#shorttitlelowercase-2-6",
+  "type": "Worksheet Exercise",
+  "number": "1.3",
+  "title": "",
+  "body": "  Flip two coins. Record the faces:   "
+},
+{
+  "id": "shorttitlelowercase-2-7",
+  "level": "2",
+  "url": "shorttitlelowercase-2.html#shorttitlelowercase-2-7",
+  "type": "Worksheet Exercise",
+  "number": "1.4",
+  "title": "",
+  "body": "  Flip two coins. Determine if they are the same:   "
+},
+{
+  "id": "shorttitlelowercase-2-8",
+  "level": "2",
+  "url": "shorttitlelowercase-2.html#shorttitlelowercase-2-8",
+  "type": "Worksheet Exercise",
+  "number": "1.5",
+  "title": "",
+  "body": "  Flip a coin until a heads appears. Record the number of flips needed:   "
+},
+{
+  "id": "shorttitlelowercase-2-9",
+  "level": "2",
+  "url": "shorttitlelowercase-2.html#shorttitlelowercase-2-9",
+  "type": "Worksheet Exercise",
+  "number": "1.6",
+  "title": "",
+  "body": "  Flip a coin until you get two heads in a row. Record how long (time) it takes:   "
+},
+{
+  "id": "shorttitlelowercase-2-10",
+  "level": "2",
+  "url": "shorttitlelowercase-2.html#shorttitlelowercase-2-10",
+  "type": "Definition",
+  "number": "1.3",
+  "title": "Event.",
+  "body": " Event   An event is a collection of outcomes in a sample space.   "
+},
+{
+  "id": "def-emptyset",
+  "level": "2",
+  "url": "shorttitlelowercase-2.html#def-emptyset",
+  "type": "Definition",
+  "number": "1.4",
+  "title": "Empty or Null Set.",
+  "body": " Empty or Null Set    denotes the empty or null set (the set containing no elements).   "
+},
+{
+  "id": "shorttitlelowercase-2-13",
+  "level": "2",
+  "url": "shorttitlelowercase-2.html#shorttitlelowercase-2-13",
+  "type": "Worksheet Exercise",
+  "number": "1.7",
+  "title": "",
+  "body": "   B indicates A is a subset of B. Draw and shade set A so it is a subset of B.      "
+},
+{
+  "id": "shorttitlelowercase-2-14",
+  "level": "2",
+  "url": "shorttitlelowercase-2.html#shorttitlelowercase-2-14",
+  "type": "Worksheet Exercise",
+  "number": "1.8",
+  "title": "",
+  "body": "   is the union of sets A and B. Elements in the union might be in A or in B or in both A and B. Shade       "
+},
+{
+  "id": "shorttitlelowercase-2-15",
+  "level": "2",
+  "url": "shorttitlelowercase-2.html#shorttitlelowercase-2-15",
+  "type": "Worksheet Exercise",
+  "number": "1.9",
+  "title": "",
+  "body": "   is the intersection of A and B. Elements in the intersection are in A and also in B. Shade .      "
+},
+{
+  "id": "shorttitlelowercase-2-16",
+  "level": "2",
+  "url": "shorttitlelowercase-2.html#shorttitlelowercase-2-16",
+  "type": "Worksheet Exercise",
+  "number": "1.10",
+  "title": "",
+  "body": "  A' is the complement of A (all elements in S that are NOT in A). Shade A'.      "
+},
+{
+  "id": "shorttitlelowercase-2-17",
+  "level": "2",
+  "url": "shorttitlelowercase-2.html#shorttitlelowercase-2-17",
+  "type": "Worksheet Exercise",
+  "number": "1.11",
+  "title": "",
+  "body": "   and are mutually exclusive or disjoint if . Make a sketch.      "
+},
+{
+  "id": "shorttitlelowercase-2-18",
+  "level": "2",
+  "url": "shorttitlelowercase-2.html#shorttitlelowercase-2-18",
+  "type": "Worksheet Exercise",
+  "number": "1.12",
+  "title": "",
+  "body": "  A and B are exhaustive if Make a sketch.      "
+},
+{
+  "id": "section-2",
+  "level": "1",
+  "url": "section-2.html",
+  "type": "Section",
+  "number": "2",
+  "title": "My Additional Section",
+  "body": " My Additional Section  tikz pics   The graph made by TikZ   A 5-cycle     bar  "
+},
+{
+  "id": "figure-tikz-example-diagram",
+  "level": "2",
+  "url": "section-2.html#figure-tikz-example-diagram",
+  "type": "Figure",
+  "number": "2.1",
+  "title": "",
+  "body": " The graph made by TikZ   A 5-cycle    "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+  this.metadataWhitelist = ['position']
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
