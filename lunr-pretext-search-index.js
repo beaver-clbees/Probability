1812144b19875e0ef8f3998c33441e354694afb1
GitHub <noreply@github.com>
2026-09-03 18:50:34 +0000

    Latest build deployed.

--- lunr-pretext-search-index.js
+++ lunr-pretext-search-index.js
@@ -6,8 +6,8 @@ var ptx_lunr_docs = [
   "url": "shorttitlelowercase-2.html",
   "type": "Worksheet",
   "number": "1",
-  "title": "Properties of Probability",
-  "body": " Properties of Probability   Random Experiment   An experiment where the outcome cannot be predicted with certainty. xyz     Sample Space   The set of all possible outcomes of a random experiment is called a sample space, S (also called outcome space, or just space).      Flip a coin and record the face that is showing:      Roll two dice and add the faces showing:       Flip two coins. Record the faces:      Flip two coins. Determine if they are the same:      Flip a coin until a heads appears. Record the number of flips needed:      Flip a coin until you get two heads in a row. Record how long (time) it takes:     Event   An event is a collection of outcomes in a sample space.    Notice that a sample space S is a set, and an event is a subset of S. We will review some basics of sets and see how they can be represented with Venn Diagrams. Our universal set is always our sample space in this context because the sample space contains all possible outcomes. We will assume all sets and elements are in S.   Empty or Null Set    denotes the empty or null set (the set containing no elements).       B indicates A is a subset of B. Draw and shade set A so it is a subset of B.          is the union of sets A and B. Elements in the union might be in A or in B or in both A and B. Shade           is the intersection of A and B. Elements in the intersection are in A and also in B. Shade .         A' is the complement of A (all elements in S that are NOT in A). Shade A'.          and are mutually exclusive or disjoint if . Make a sketch.         A and B are exhaustive if Make a sketch.       These definitions extend to larger numbers of subsets. For example, given     are mutually exclusive events if for (they are pairwise disjoint).     are exhaustive events if      "
+  "title": "Basic Definitions",
+  "body": " Basic Definitions   Random Experiment   An experiment where the outcome cannot be predicted with certainty. xyz     Sample Space   The set of all possible outcomes of a random experiment is called a sample space, S (also called outcome space, or just space).      Flip a coin and record the face that is showing:      Roll two dice and add the faces showing:       Flip two coins. Record the faces:      Flip two coins. Determine if they are the same:      Flip a coin until a heads appears. Record the number of flips needed:      Flip a coin until you get two heads in a row. Record how long (time) it takes:     Event   An event is a collection of outcomes in a sample space.    Notice that a sample space S is a set, and an event is a subset of S. We will review some basics of sets and see how they can be represented with Venn Diagrams. Our universal set is always our sample space in this context because the sample space contains all possible outcomes. We will assume all sets and elements are in S.   Empty or Null Set    denotes the empty or null set (the set containing no elements).       B indicates A is a subset of B. Draw and shade set A so it is a subset of B.          is the union of sets A and B. Elements in the union might be in A or in B or in both A and B. Shade           is the intersection of A and B. Elements in the intersection are in A and also in B. Shade .         A' is the complement of A (all elements in S that are NOT in A). Shade A'.          and are mutually exclusive or disjoint if . Make a sketch.         A and B are exhaustive if Make a sketch.       These definitions extend to larger numbers of subsets. For example, given     are mutually exclusive events if for (they are pairwise disjoint).     are exhaustive events if      We are interested in computing the probability of an event A in our sample space. We can approximate the probability by repeating the experiment n times. Let N(A) be the number of times an outcome of the experiment is in the event (subset) A. Then we can approximate the probability of A as        which is called the relative frequency of the event A in these n repetitions of the experiment.  Note that if n is small and a few of us do this experiment we may get wildly different answers. However as n gets larger and larger, this fraction will tend to stabilize and we call it the probability of event A . You can think of it as    (Probability of A)       Suppose we have a fair, six sided die and we want to know the probability of the event, E that it will show an even number. Find the sample space S and E. What do you expect for P(E)?    Now we give the formal definition of a probability function.   Probability   Probability is a real-valued set function, P, that assigns to each event A in the sample space S, a number P(A), called the probability of the event A such that the following properties are satisfied:             If are events and (mutually disjoint) then for each positive integer k and for any countable (possibly infinite) number of events.         Sometimes you can use mathematics to determine what the probability function should be under given conditions. For example in the 6-sided die example, if the die is fair, then Let Let G be the set of odd outcomes.  Find      Equally Likely Outcomes   Suppose we have a random experiment with m outcomes and sample space . If each of the outcomes in S has the same probability of occurring (e.g. roll a fair die), then we say that each of the m outcomes are equally likely. In that case,         This makes it easy to determine the probability of an event: If is an event, with h outcomes, then    Standard deck of cards      Draw one card at random from a standard 52 card deck. The sample space is the collection of 52 cards. Assume that each card is equally likely to be chosen (i.e., that a card is drawn `at random'). Thus the probability function assigns a probability of to each of the outcomes. Let >                     Determine the following probabilities.                               Suppose a dart is thrown at the picture below. Assume it lands in the sample space, S. Let A be the event the dart lands in rectangle A and let B be the event the dart lands in rectangle B. Estimate the following probabilities and be prepared to justify your estimates.                                        "
 },
 {
   "id": "def-ranexp",
@@ -154,22 +154,85 @@ var ptx_lunr_docs = [
   "body": "  A and B are exhaustive if Make a sketch.      "
 },
 {
-  "id": "section-2",
-  "level": "1",
-  "url": "section-2.html",
-  "type": "Section",
-  "number": "2",
-  "title": "My Additional Section",
-  "body": " My Additional Section  tikz pics   The graph made by TikZ   A 5-cycle     bar  "
+  "id": "shorttitlelowercase-2-21",
+  "level": "2",
+  "url": "shorttitlelowercase-2.html#shorttitlelowercase-2-21",
+  "type": "Worksheet Exercise",
+  "number": "1.13",
+  "title": "",
+  "body": "     "
 },
 {
-  "id": "figure-tikz-example-diagram",
+  "id": "shorttitlelowercase-2-24",
   "level": "2",
-  "url": "section-2.html#figure-tikz-example-diagram",
-  "type": "Figure",
-  "number": "2.1",
+  "url": "shorttitlelowercase-2.html#shorttitlelowercase-2-24",
+  "type": "Worksheet Exercise",
+  "number": "1.14",
   "title": "",
-  "body": " The graph made by TikZ   A 5-cycle    "
+  "body": "  (Probability of A)    "
+},
+{
+  "id": "shorttitlelowercase-2-25",
+  "level": "2",
+  "url": "shorttitlelowercase-2.html#shorttitlelowercase-2-25",
+  "type": "Worksheet Exercise",
+  "number": "1.15",
+  "title": "",
+  "body": "  Suppose we have a fair, six sided die and we want to know the probability of the event, E that it will show an even number. Find the sample space S and E. What do you expect for P(E)?   "
+},
+{
+  "id": "def-probability",
+  "level": "2",
+  "url": "shorttitlelowercase-2.html#def-probability",
+  "type": "Definition",
+  "number": "1.5",
+  "title": "Probability.",
+  "body": " Probability   Probability is a real-valued set function, P, that assigns to each event A in the sample space S, a number P(A), called the probability of the event A such that the following properties are satisfied:             If are events and (mutually disjoint) then for each positive integer k and for any countable (possibly infinite) number of events.      "
+},
+{
+  "id": "shorttitlelowercase-2-28",
+  "level": "2",
+  "url": "shorttitlelowercase-2.html#shorttitlelowercase-2-28",
+  "type": "Worksheet Exercise",
+  "number": "1.16",
+  "title": "",
+  "body": "  Sometimes you can use mathematics to determine what the probability function should be under given conditions. For example in the 6-sided die example, if the die is fair, then Let Let G be the set of odd outcomes.  Find    "
+},
+{
+  "id": "shorttitlelowercase-2-31",
+  "level": "2",
+  "url": "shorttitlelowercase-2.html#shorttitlelowercase-2-31",
+  "type": "Worksheet Exercise",
+  "number": "1.17",
+  "title": "",
+  "body": "      "
+},
+{
+  "id": "deck of cards",
+  "level": "2",
+  "url": "shorttitlelowercase-2.html#deck of cards",
+  "type": "Figure",
+  "number": "1.6",
+  "title": "",
+  "body": " Standard deck of cards  "
+},
+{
+  "id": "shorttitlelowercase-2-35",
+  "level": "2",
+  "url": "shorttitlelowercase-2.html#shorttitlelowercase-2-35",
+  "type": "Worksheet Exercise",
+  "number": "1.18",
+  "title": "",
+  "body": "  Draw one card at random from a standard 52 card deck. The sample space is the collection of 52 cards. Assume that each card is equally likely to be chosen (i.e., that a card is drawn `at random'). Thus the probability function assigns a probability of to each of the outcomes. Let >                     Determine the following probabilities.                            "
+},
+{
+  "id": "shorttitlelowercase-2-36",
+  "level": "2",
+  "url": "shorttitlelowercase-2.html#shorttitlelowercase-2-36",
+  "type": "Worksheet Exercise",
+  "number": "1.19",
+  "title": "",
+  "body": "  Suppose a dart is thrown at the picture below. Assume it lands in the sample space, S. Let A be the event the dart lands in rectangle A and let B be the event the dart lands in rectangle B. Estimate the following probabilities and be prepared to justify your estimates.                                       "
 }
 ]
 
